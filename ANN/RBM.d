369eb5a407411b710e16c41cc811c8616af4302f
James Watson <james.watson-2@colorado.edu>
2023-02-02 20:43:50 -0700

    fixed the learning rule

--- ANN/RBM.d
+++ ANN/RBM.d
@@ -2,12 +2,13 @@ module RBM;
 
 /*
 RBM.d
-James Watson, 2023-01
+James Watson, 2023-02
 Simplest Restricted Boltzmann Machine (RBM) example
 From:
+* https://medium.com/machine-learning-researcher/boltzmann-machine-c2ce76d94da5
 * Hinton, Geoffrey E. "A practical guide to training restricted Boltzmann machines." 
   Neural Networks: Tricks of the Trade: Second Edition (2012): 599-619.
-* FIXME: LINK YT LECTURES
+
 
 rdmd RBM.d
 */
@@ -104,12 +105,14 @@ struct RBM{
         x = alloc_array!float( dI );
     }
 
+
     void set_input( float[] input ){
         // Populate the input vector
         for( uint k = 0; k < dI; k++ ){
             x[k] = input[k];
         }
     }
+
 
     void load_visible(){
         // Populate the visible units with the input vector
@@ -118,6 +121,7 @@ struct RBM{
         }
 
     }
+
 
     float energy(){
         // Calc the (binary) energy function given the current network state
@@ -139,45 +143,61 @@ struct RBM{
         return netNRG + inpNRG + hidNRG;
     }
 
-    float p_h_given_x(){
+
+    float p_hj_given_v( uint j ){
+        // Probability of the `j`th hidden neuron being active given the visible neuron activation
+        float p_j = b[j];
+        for( uint k = 0; k < dI; k++ ){
+            p_j += v[k] * W[k][j];
+        }
+        return sigmoid( p_j );
+    }
+
+
+    float p_hj_given_x( uint j ){
+        // Probability of the `j`th hidden neuron being active given the input vector
+        float p_j = b[j];
+        for( uint k = 0; k < dI; k++ ){
+            p_j += x[k] * W[k][j];
+        }
+        return sigmoid( p_j );
+    }
+
+
+    float p_vk_given_h( uint k ){
+        // Probability of the `j`th hidden neuron being active given the input vector
+        float p_k = c[k];
+        for( uint j = 0; j < dH; j++ ){    
+            p_k += W[k][j] * h[j];
+        }
+        return sigmoid( p_k );
+    }
+
+
+    float p_h_given_v(){
         // Conditional probabolity of the current hidden values given the current input
         float rtnProd = 1.0;
-        float dotProd = 0.0;
         for( uint j = 0; j < dH; j++ ){
-            dotProd = 0.0;
-            for( uint k = 0; k < dI; k++ ){
-                dotProd += W[k][j] * v[k];
-            }
-            rtnProd *= sigmoid( b[j] + dotProd );
+            rtnProd *= p_hj_given_v(j);
         }
         return rtnProd;
     }
 
-    float p_x_given_h(){
+
+    float p_v_given_h(){
         // Conditional probabolity of the current input given the current hidden values
         float rtnProd = 1.0;
-        float dotProd = 0.0;
         for( uint k = 0; k < dI; k++ ){
-            dotProd = 0.0;
-            for( uint j = 0; j < dH; j++ ){    
-                dotProd += W[k][j] * h[j];
-            }
-            rtnProd *= sigmoid( c[k] + dotProd );
+            rtnProd *= p_vk_given_h(k);
         }
         return rtnProd;
     }
 
+
     void Gibbs_sample_visible(){
-        // Run Gibbs sampling on the visibile units for `n` steps, `n` wraps
-        float p_l = 0.0;
-
+        // Run Gibbs sampling on all visibile units
         for( uint k = 0; k < dI; k++ ){
-            p_l = c[k];
-            for( uint j = 0; j < dH; j++ ){     
-                p_l += h[j] * W[k][j];
-            }
-
-            if( uniform( 0.0f, 1.0f, rnd ) <= p_l )
+            if( uniform( 0.0f, 1.0f, rnd ) <= p_vk_given_h(k) )
                 v[k] = 1.0;
             else
                 v[k] = 0.0;
@@ -191,26 +211,24 @@ struct RBM{
         /* Section 3.1: Assuming that the hidden units are binary and that you are using Contrastive Divergence, 
         the hidden units should have stochastic binary states when they are being driven by a data-vector. 
         The probability of turning on ahiddenunit,j, is computed by applying the logistic function */
-
-        float p_j = 0.0;
-
         for( uint j = 0; j < dH; j++ ){    
-            p_j = b[j];
-            for( uint k = 0; k < dI; k++ ){
-                p_j += v[k] * W[k][j];
-            }
-
-            if( uniform( 0.0f, 1.0f, rnd ) <= p_j )
+            if( uniform( 0.0f, 1.0f, rnd ) <= p_hj_given_v(j) )
                 h[j] = 1.0;
             else
                 h[j] = 0.0;
         }
-
     }
+
+
+    
+
 
     void Contrastive_Divergence_iter(){
         // Run a single iteration of (Persistent) Contrastive Divergence
-        
+        float p_hjv = 0.0;
+        float p_hjx = 0.0;
+
+
         // 1. Generate a negative sample using n steps of Gibbs sampling
         /* One iteration of alternating Gibbs sampling consists of updating all of the hidden units 
         in parallel followed by updating all of the visible units in parallel. */
@@ -218,12 +236,17 @@ struct RBM{
         Gibbs_sample_visible();
 
         // 2. Update parameters
-        // FIXME : THIS IS WRONG
         for( uint j = 0; j < dH; j++ ){
+            p_hjv = p_hj_given_v(j);
+            p_hjx = p_hj_given_x(j);
             for( uint k = 0; k < dI; k++ ){
-                W[k][j] += lr * (h[j]*x[k]*x[k] - h[j]*v[k]*v[k]);
+                W[k][j] += lr * (p_hjx*x[k] - p_hjv*v[k]);
             }
-            
+            b[j] += lr * (p_hjx - p_hjv);
+        }
+
+        for( uint k = 0; k < dI; k++ ){
+            c[k] += lr * (x[k] - v[k]);
         }
     }
 }
